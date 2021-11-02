9fb0ee1c075bcabe2e55b57bba166ad8ab045c35
mauriciozaccaro <mauriciozaccaro@gmail.com>
2021-11-02 11:52:32 -0400

    User entity and resource

---
+++ src/main/java/com/systemmz/courses/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.systemmz.courses.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.systemmz.courses.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping // Indica que o método responde a uma requisição GET do HTTP
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Mauricio Neto", "neto.mauricio@unemat.br", "(66) 3439-5931", "264492");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}
